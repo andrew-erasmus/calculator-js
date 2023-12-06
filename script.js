c686f3ca09bc8750ac754566c8732e3bbb9cbd01
andrew-erasmus <andrew.r.eras@gmail.com>
2023-12-06 11:32:29 +0200

    Add display changes for button input

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //Button click handler
 let eqn = '';
-
 let buttons = document.querySelectorAll('button');
 buttons = Array.from(buttons);
 
 let clearBtn = document.getElementById("ac-button");
-
-
+let display = document.getElementById('equation');
+display.style.fontSize = '40px';
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         eqn += e.target.innerHTML;
+        display.innerHTML = eqn;
         console.log(eqn);
         
     });
@@ -18,6 +18,7 @@ buttons.forEach(button => {
 
 clearBtn.onclick = () => {
     eqn = '';
+    display.innerHTML = eqn;
     console.log(eqn);
 };
 
