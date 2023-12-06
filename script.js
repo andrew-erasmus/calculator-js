472a3aaedecf8dd689a2cded74e7a10b4c3c8cf2
andrew-erasmus <andrew.r.eras@gmail.com>
2023-12-06 14:05:27 +0200

    Add delete button functionality

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let equals = document.getElementById('equals');
 let eqnArr = [];
 let eqn='';
+let deleteBtn = document.getElementById("delete");
 let clearBtn = document.getElementById("ac-button");
 let display = document.getElementById('equation');
 let buttons = document.querySelectorAll('button');
-
 buttons = Array.from(buttons);
 display.style.fontSize = '40px';
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-        if(e.target.innerHTML != '='){
+        if(e.target.innerHTML != '=' && e.target.innerHTML != 'Del'){
         eqn += e.target.innerHTML;
-        display.innerHTML = eqn;
+        updateDisplay()
         console.log(eqn);
         }
     });
@@ -20,13 +20,23 @@ buttons.forEach(button => {
 
 clearBtn.onclick = () => {
     eqn = '';
-    display.innerHTML = eqn;
+    updateDisplay()
     console.log(eqn);
 };
 
 equals.onclick = () => {
     eqn = String(display.innerHTML);
     eqnArr = toArray(eqn);
+    answer=compute(eqnArr);
+}
+
+deleteBtn.onclick = () => {
+    eqn = eqn.slice(0,eqn.length-1);
+    updateDisplay()
+}
+
+function updateDisplay(){
+    display.innerHTML = eqn;
 }
 
 function toArray(equation) {
@@ -38,7 +48,6 @@ function toArray(equation) {
             let j = i;
             let found = false;
             while (found === false) {
-                console.log(parseInt(equation.charAt(j)));
                 if (isNaN(parseInt(equation.charAt(j)))) {
                     found = true;
                     num = parseInt(num);
