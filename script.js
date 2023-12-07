dd522f3cac7078df3e5d3203db7793129b3ece4f
Andrew Erasmus <andrew.r.eras@gmail.com>
2023-12-07 14:22:25 +0200

    Add operate method

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let equals = document.getElementById('equals');
 let eqnArr = [];
-let eqn='';
+let eqn = '';
 let deleteBtn = document.getElementById("delete");
 let clearBtn = document.getElementById("ac-button");
 let display = document.getElementById('equation');
@@ -10,59 +10,79 @@ display.style.fontSize = '40px';
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-        if(e.target.innerHTML != '=' && e.target.innerHTML != 'Del'){
-        eqn += e.target.innerHTML;
-        updateDisplay()
-        console.log(eqn);
+        if (e.target.innerHTML != '=' && e.target.innerHTML != 'Del') {
+            eqn += e.target.innerHTML;
+            updateDisplay(eqn)
+            console.log(eqn);
         }
     });
 });
 
 clearBtn.onclick = () => {
     eqn = '';
-    updateDisplay()
+    updateDisplay(eqn)
     console.log(eqn);
 };
 
 equals.onclick = () => {
     eqn = String(display.innerHTML);
-    eqnArr = toArray(eqn);
-    answer=compute(eqnArr);
 }
 
 deleteBtn.onclick = () => {
-    eqn = eqn.slice(0,eqn.length-1);
-    updateDisplay()
+    eqn = eqn.slice(0, eqn.length - 1);
+    updateDisplay(eqn)
 }
 
-function updateDisplay(){
+function updateDisplay(eqn) {
     display.innerHTML = eqn;
 }
 
-function toArray(equation) {
-    let num = '';
-    for (let i = 0; i < equation.length; i++) {
-        if (isNaN(parseInt(equation.charAt(i)))) {
-            eqnArr.push(equation.charAt(i));
-        } else {
-            let j = i;
-            let found = false;
-            while (found === false) {
-                if (isNaN(parseInt(equation.charAt(j)))) {
-                    found = true;
-                    num = parseInt(num);
-                    eqnArr.push(num);
-                    num='';
-                    i = j - 1;
-                    break;
-                }
-                num += String(equation.charAt(j));
-                j++;
-            }
-        }
+
+// FIX ISSUE WHERE A POINT IS NOT INCLUDED FOR THE DOUBLES
+// function toArray(equation) {
+//     let num = '';
+//     for (let i = 0; i < equation.length; i++) {
+//         if (isNaN(parseInt(equation.charAt(i)))) {
+//             eqnArr.push(equation.charAt(i));
+//         } else {
+//             let j = i;
+//             let found = false;
+//             while (found === false) {
+//                 if (isNaN(parseInt(equation.charAt(j)))) {
+//                     found = true;
+//                     num = parseInt(num);
+//                     eqnArr.push(num);
+//                     num = '';
+//                     i = j - 1;
+//                     break;
+//                 }
+//                 num += String(equation.charAt(j));
+//                 j++;
+//             }
+//         }
+//     } console.log(eqnArr);
+//     return eqnArr;
+// }
+
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+        case '\u00D7':
+            multiply(a, b);
+            break;
+        case '\u00F7':
+            divide(a,b);
+            break;
+        case '\u002B':
+            add(a,b);
+            break;
+        case '\u2212':
+            subtract(a,b);
+            break;
     }
-    return eqnArr;
 }
+
 
 function setAnswer(answer) {
     display.innerHTML = answer;
